5d5684a9b49bbc72066960935a84766e548453f2
peter <peter.shenlq@gmail.com>
2016-03-24 23:07:00 +0800

    UPDATE: 当Group有多个子节点（Groupt-Item）时，用border分隔开，并缩进1rem。

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,6 +1,33 @@
 <template>
 	<group>
+		<group-item>
+			<i class="fa fa-lg fa-bluetooth-b" slot="icon"></i>
+			<span slot="body">Group</span>
+			<span slot="desc">0.1.0</span>
+		</group-item>
+				<group-item>
+			<i class="fa fa-lg fa-bluetooth-b" slot="icon"></i>
+			<span slot="body">Group</span>
+			<span slot="desc">0.1.0</span>
+		</group-item>
+				<group-item>
+			<i class="fa fa-lg fa-bluetooth-b" slot="icon"></i>
+			<span slot="body">Group</span>
+			<span slot="desc">0.1.0</span>
+		</group-item>
+	</group>
+	<group>
 		<group-item link="#">
+			<i class="fa fa-lg fa-bluetooth-b" slot="icon"></i>
+			<span slot="body">Group</span>
+			<span slot="desc">0.1.0</span>
+		</group-item>
+				<group-item link="#">
+			<i class="fa fa-lg fa-bluetooth-b" slot="icon"></i>
+			<span slot="body">Group</span>
+			<span slot="desc">0.1.0</span>
+		</group-item>
+				<group-item link="#">
 			<i class="fa fa-lg fa-bluetooth-b" slot="icon"></i>
 			<span slot="body">Group</span>
 			<span slot="desc">0.1.0</span>
