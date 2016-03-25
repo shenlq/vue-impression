bd0f90ad4942edc895b7a938ab167be2ea91f8b6
peter <peter.shenlq@gmail.com>
2016-03-25 19:03:47 +0800

    RESTRUCTURE: 重构input-text组件，从group-item中剥离出去。

--- src/components/InputText.vue
+++ src/components/InputText.vue
@@ -1,22 +1,20 @@
 <template>
-	<div class="group">
-		<div class="group-item">
-	  	    <div class="groupt-item-heading" v-if="_slotContents.default">
-	            <slot></slot>
-	       	</div>
-	      	<div class="group-item-body">
-				<input class="input" :type="dtype" v-model="dValue"/>
-	      	</div>
-	      	<div class="group-item-footer">
-	      		<a href="javascript:void(0);" class="text-muted" @click="clearHandle" v-if="clear">
-	      			<i class="fa fa-times"></i>
-	      		</a>
-	      		<i class="fa fa-exclamation-triangle text-warning" v-if="isWarning"></i>
-	      		<i class="fa fa-exclamation-circle text-danger" v-if="isError"></i>
-	      		<i class="fa fa-check text-success" v-if="isSuccess"></i>
-	      	</div>
-	  	</div>
-	</div>
+	<div class="input-item">
+  	    <div class="groupt-item-heading" v-if="_slotContents.default">
+            <slot></slot>
+       	</div>
+      	<div class="group-item-body">
+			<input class="input" :type="dtype" v-model="dValue"/>
+      	</div>
+      	<div class="group-item-footer">
+      		<a href="javascript:void(0);" class="text-muted" @click="clearHandle" v-if="clear">
+      			<i class="fa fa-times"></i>
+      		</a>
+      		<i class="fa fa-exclamation-triangle text-warning" v-if="isWarning"></i>
+      		<i class="fa fa-exclamation-circle text-danger" v-if="isError"></i>
+      		<i class="fa fa-check text-success" v-if="isSuccess"></i>
+      	</div>
+  	</div>
 </template>
 
 <script>
@@ -52,9 +50,6 @@
 				this.dValue = "";
 				this.isSuccess = this.isWarning = this.isError = false;
 			}
-		},
-		created(){
-			console.log(this.isSuccess, this.isWarning, this.isError);
 		}
 	};
 </script>
