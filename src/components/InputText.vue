6e8ebe48b55456109444e19a44d29421b3020821
peter <peter.shenlq@gmail.com>
2016-03-25 18:25:47 +0800

    ADD: 给input-text组件添加【清空】按钮和状态。目前状态包括：success、warning和error。

--- src/components/InputText.vue
+++ src/components/InputText.vue
@@ -5,7 +5,15 @@
 	            <slot></slot>
 	       	</div>
 	      	<div class="group-item-body">
-				<input class="input" :type="type"/>
+				<input class="input" :type="dtype" v-model="dValue"/>
+	      	</div>
+	      	<div class="group-item-footer">
+	      		<a href="javascript:void(0);" class="text-muted" @click="clearHandle" v-if="clear">
+	      			<i class="fa fa-times"></i>
+	      		</a>
+	      		<i class="fa fa-exclamation-triangle text-warning" v-if="isWarning"></i>
+	      		<i class="fa fa-exclamation-circle text-danger" v-if="isError"></i>
+	      		<i class="fa fa-check text-success" v-if="isSuccess"></i>
 	      	</div>
 	  	</div>
 	</div>
@@ -14,8 +22,23 @@
 <script>
 	export default {
 		props: {
-            type: {
+            dtype: {
                 type: String
+            },
+            clear: {
+            	type: Boolean
+            },
+            dValue: {
+            	type: String
+            },
+            isSuccess: {
+            	type: Boolean
+            },
+            isWarning: {
+            	type: Boolean
+            },
+            isError: {
+            	type: Boolean
             }
         },
 		data(){
@@ -23,8 +46,15 @@
 				type: this.type || 'text'
 			}
 		},
+		methods: {
+			//清空
+			clearHandle(){
+				this.dValue = "";
+				this.isSuccess = this.isWarning = this.isError = false;
+			}
+		},
 		created(){
-			console.log(this);
+			console.log(this.isSuccess, this.isWarning, this.isError);
 		}
 	};
 </script>
--- src/views/input.vue
+++ src/views/input.vue
@@ -1,13 +1,35 @@
 <template>
-	<group-title>Input base</group-title>
+	<group-title>Input default</group-title>
 	<input-text>
 		<label slot>username:</label>
 	</input-text>
-	<input-text>
+
+	<group-title>Input password</group-title>
+	<input-text dtype="password">
 		<label slot>password:</label>
 	</input-text>
-	<group-title>With clear button</group-title>
 
+	<group-title>With clear icon</group-title>
+	<input-text clear>
+		<label slot>clear:</label>
+	</input-text>
+
+	<group-title>Input status</group-title>
+	<input-text is-success>
+		<label slot>success:</label>
+	</input-text>
+	<input-text is-warning>
+		<label slot>warning:</label>
+	</input-text>
+	<input-text is-error>
+		<label slot>error:</label>
+	</input-text>
+
+	<group-title>Multiple icon</group-title>
+
+	<input-text is-warning clear>
+		<label slot>warning:</label>
+	</input-text>
 </template>
 
 
