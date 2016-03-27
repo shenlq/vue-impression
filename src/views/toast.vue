11413e1c78084a7c4c936fec972388aa7a235fb1
peter <peter.shenlq@gmail.com>
2016-03-27 20:51:35 +0800

    UPDATE: 增加toast类型，包括success、warning和error。

--- src/views/toast.vue
+++ src/views/toast.vue
@@ -8,13 +8,27 @@
 		<container-body>
 			<group>
 				<group-item :click="defaultChangeHandle">
-					<span slot="body">默认（1s后消失）</span>
+					<span slot="body">默认（1s）</span>
 				</group-item>
 				<group-item :click="customChangeHandle">
-					<span slot="body">自定义（3s后消失）</span>
+					<span slot="body">自定义（3s）</span>
 				</group-item>
 			</group>
-			<toast type="error" :show.sync="showToast" :duration="duration">操作失败</toast>
+			<group>
+				<group-item :click="successTypeHandle">
+					<i class="fa fa-lg fa-check text-success" slot="heading"></i>
+					<span slot="body">成功</span>
+				</group-item>
+				<group-item :click="warningTypeHandle">
+					<i class="fa fa-lg fa-exclamation-triangle text-warning" slot="heading"></i>
+					<span slot="body">警告</span>
+				</group-item>
+				<group-item :click="errorTypeHandle">
+					<i class="fa fa-lg fa-exclamation-circle text-danger" slot="heading"></i>
+					<span slot="body">失败</span>
+				</group-item>
+			</group>
+			<toast :type="totastType" :show.sync="showToast" :duration="duration">{{totastMsg}}</toast>
 		</container-body>
 	</container>
 </template>
@@ -36,19 +50,44 @@
 		data(){
 			return {
 				showToast: false,
-				duration: 1000
+				duration: 1000,
+				totastType: 'success',
+				totastMsg: '操作成功'
 			}
 		},
 		methods: {
 			//显示默认
 			defaultChangeHandle(event){
 				this.duration = 1000;
+				this.totastMsg = '1S后消失';
 				this.showToast = !this.showToast;
 			},
 			//显示默认
 			customChangeHandle(event){
 				this.duration = 3000;
+				this.totastMsg = '3S后消失';
 				this.showToast = !this.showToast;
+			},
+			//成功提示
+			successTypeHandle(){
+				this.duration = 1000;
+				this.totastType = 'success';
+				this.showToast = !this.showToast;
+				this.totastMsg = '操作成功';
+			},
+			//警告提示
+			warningTypeHandle(){
+				this.duration = 1000;
+				this.totastType = 'warning';
+				this.showToast = !this.showToast;
+				this.totastMsg = '操作警告';
+			},
+			//操作失败
+			errorTypeHandle(){
+				this.duration = 1000;
+				this.totastType = 'error';
+				this.showToast = !this.showToast;
+				this.totastMsg = '操作失败';
 			}
 		}
 	};
