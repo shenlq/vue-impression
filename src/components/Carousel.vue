33465a42ec34ddddb044b3f16d568144e95a7828
peter <peter.shenlq@gmail.com>
2016-04-05 16:34:10 +0800

    UPDATE: 完善carousel组件，支持超链接。

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,5 +1,8 @@
 <template>
 	<div class="carousel">
+		<ol class="carousel-indicators">
+			<li :class="{'active': index == activeIndex}" v-for="(index, item) in images"></li>
+		</ol>
 		<div :class="['carousel-item',
 			index==activeIndex?'active':'',
 			(index - activeIndex + images.length)%images.length == 1?'next':'',
@@ -9,7 +12,10 @@
 			action=='next' && (index - activeIndex + images.length)%images.length == 1?'left':'',
 			action=='prev' && (index - activeIndex + images.length)%images.length == 2?'right':''
 			]" v-for="(index, item) in images">
-			<img class='carousel-img' :src="item"/>
+			<a v-if="item.href" :href="item.href" target="_blank">
+				<img class='carousel-img' :src="item.src"/>
+			</a>
+			<img v-else class='carousel-img' :src="item.src"/>
 		</div>
 	</div>
 </template>
@@ -22,7 +28,16 @@
 				default: 3000
 			},
 			images: {
-				type: Array
+				type: Array,
+				coerce(val){
+					return val.map((item, index) => {
+						if("[object Object]" == Object.prototype.toString.call(item)){
+							return item;
+						}else{
+							return {src: item};
+						};
+					});
+				}
 			},
 			debounce: {
 				default: 600
