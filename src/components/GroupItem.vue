1a1500c434cfd7835eb2c587259fa14948463679
peter <peter.shenlq@gmail.com>
2016-04-12 17:18:42 +0800

    BUGFIX: 修复了一个伟大的bug，就是关于事件的绑定，原来是通过:click来传递，现在通过@click来传递，允许回调函数可以附带自定义参数。

--- src/components/GroupItem.vue
+++ src/components/GroupItem.vue
@@ -1,18 +1,18 @@
 <template>
-  	<div :class="class" v-if="!vLink" @click="!!click && click($event)">
-  	    <span class="group-item-heading" v-if="_slotContents.heading">
+    <div :class="class" v-if="!vLink" @click="click">
+        <span class="group-item-heading" v-if="_slotContents.heading">
             <slot name="heading"></slot>
-       	</span>
-      	<span class="group-item-body">
+        </span>
+        <span class="group-item-body">
             <slot></slot>
-      	</span>
-      	<span class="group-item-footer">
+        </span>
+        <span class="group-item-footer">
             <slot name="footer"></slot>
         </span>
         <template v-else>
         </template>
-        <span class="group-item-arrow" v-if="click"></span>
-  	</div>
+        <span class="group-item-arrow" v-if="_events.click"></span>
+    </div>
 
     <a :class="class" v-if="vLink" v-link="vLink">
         <span class="group-item-heading" v-if="_slotContents.heading">
@@ -32,18 +32,25 @@
     export default {
         props: {
             vLink: Object,
-            click: {
-                type: Function
-            },
             class: {
                 type: Array,
                 default: '',
                 coerce(val){
-                  let result = ['group-item'];
-                  val &&  (result = result.concat(val.split(' ')));
-                  return result;
+                    let result = ['group-item'];
+                    val &&  (result = result.concat(val.split(' ')));
+                    return result;
                 }
             }
-        }
+        },
+        methods: {
+            click(){
+                if(!this._events || !this._events.click || this._events.click.length < 1){
+                    return false;
+                }
+                this._events.click.forEach((fn, index) => {
+                    fn();
+                });
+            }
+        },
     }
 </script>
