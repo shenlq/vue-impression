2458ce08d525ee77cedd1e4236dc07f34803ddef
peter <peter.shenlq@gmail.com>
2016-04-14 16:32:54 +0800

    TEST: 容器下拉刷新。

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -1,5 +1,5 @@
 <template>
-    <div :class="['container', bottom?'container-sm':'']">
+    <div :class="['container', bottom?'container-sm':'']"  @touchstart.self="touchstart" @touchmove.self="touchmove" @touchend.self="touchend">
         <slot></slot>
     </div>
 </template>
@@ -10,6 +10,19 @@
 			bottom: {
 				type: Boolean,
 				default: false
+			}
+		},
+		methods: {
+			touchstart(event){
+				this._touchStartY = event.touches[0].screenY;
+			},
+			touchmove(event){
+				let touchY = event.touches[0].screenY;
+				let marginTop = touchY - this._touchStartY;
+				marginTop <= 38 && (event.target.style.marginTop =marginTop);
+			},
+			touchend(event){
+				event.target.style.marginTop = 0;
 			}
 		}
 	};
