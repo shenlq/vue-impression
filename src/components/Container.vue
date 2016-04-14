da94bd3878ce0ff52275df5c5d6607088cbe67d9
peter <peter.shenlq@gmail.com>
2016-04-14 21:01:06 +0800

    UPDATE: 漂亮的下拉刷新。

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -2,38 +2,69 @@
     <div :class="['container', bottom?'container-sm':'']"  @touchstart="touchstart" @touchmove="touchmove" @touchend="touchend">
         <slot></slot>
 	    <div class="droploading">
-	    	<p>
+	    	<div class="droploading-heading">
+	    		<checkbox checked circle outline>
+					全场包邮
+				</checkbox>
+				<checkbox checked circle outline>
+					达达品质
+				</checkbox>
+				<checkbox checked circle outline>
+					7天包退
+				</checkbox>
+	    	</div>
+	    	<div class="droploading-image">
 				<img src="../images/loading.gif">
-	    	</p>
-			<p>↓ 下拉刷新</p>
+	    	</div>
+	    	<div class="droploading-footer">
+	    		{{ TOUCH_GUIDE[guide] }}
+	    	</div>
 		</div>
     </div>
 </template>
 
 <script>
+	import Checkbox from './Checkbox';
+
 	export default {
+		components: {
+			Checkbox
+		},
 		props: {
 			bottom: {
 				type: Boolean,
 				default: false
 			}
 		},
+		data(){
+			return {
+				guide: 0,
+				TOUCH_GUIDE: ['↓ 下拉刷新', '↑ 释放刷新', '正在刷新...']
+			};
+		},
 		methods: {
 			touchstart(event){
 				this._touchStartY = event.touches[0].screenY;
-				console.log(this);
 			},
 			touchmove(event){
 				this.$el.style.overflow = 'visible';
 				let touchY = event.touches[0].screenY;
-				let marginTop = touchY - this._touchStartY;
-				marginTop <= 100 && (this.$el.style.marginTop =marginTop);
+				this._touchRange = touchY - this._touchStartY;
+				this._touchRange >= 110 && (this.guide = 1);
+				this._touchRange <= 172 && (this.$el.style.marginTop = this._touchRange);
 			},
 			touchend(event){
+				this._touchRange > 110 && (this.$el.style.marginTop = 110);
+				if(this._touchRange < 110){
+					this.$el.style.overflow = 'scroll';
+					this.$el.style.marginTop = 0;
+					return false;
+				}
+				this.guide = 2;
 				setTimeout(() => {
 					this.$el.style.overflow = 'scroll';
 					this.$el.style.marginTop = 0;
-				}, 2000);
+				}, 1500);
 			}
 		}
 	};
