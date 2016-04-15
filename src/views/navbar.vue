e7b94a60ddba13b42888f0cf781e8deb53d11e73
peter <peter.shenlq@gmail.com>
2016-04-15 17:53:22 +0800

    BUGFIX: 导航栏布局问题修复。

--- src/views/navbar.vue
+++ src/views/navbar.vue
@@ -9,7 +9,7 @@
 		<a v-link="{name:'demo'}"><i class="fa fa-arrow-left"></i></a>
 		<span class="split"></span>
 		<span>转账</span>
-		<span class="pull-right">
+		<span class="flex-item text-right">
 			<a href="#"><i class="fa fa-search"></i></a>
 			<a href="#"><i class="fa fa-plus"></i></a>
 			<a href="#"><i class="fa fa-ellipsis-v"></i></a>
@@ -19,7 +19,7 @@
 		<a v-link="{name:'demo'}"><i class="fa fa-arrow-left"></i></a>
 		<span class="split"></span>
 		<span>设置</span>
-		<span class="pull-right">
+		<span class="flex-item text-right">
 			<i class="fa fa-search"></i>
 			<i class="fa fa-plus"></i>
 			<i class="fa fa-ellipsis-v"></i>
