86d6de21dcf36173be84a13c4defde7f2323ba0d
peter <peter.shenlq@gmail.com>
2016-04-21 10:10:20 +0800

    UPDATE: tab-bottom组件选中事件属性添加。

--- src/views/footer.vue
+++ src/views/footer.vue
@@ -4,41 +4,41 @@
 		<span class="split"></span>
 		<span>Tab bottom</span>
 	</navbar>
-	<tab-bottom :style="style1">
-		<tab-item>电话</tab-item>
-		<tab-item>短信</tab-item>
-		<tab-item>我</tab-item>
-		<tab-item>照相</tab-item>
+	<tab-bottom :on-select="tabChangeHandle" :style="style1">
+		<tab-item event-key="电话">电话</tab-item>
+		<tab-item event-key="短信">短信</tab-item>
+		<tab-item event-key="我">我</tab-item>
+		<tab-item event-key="照相">照相</tab-item>
 	</tab-bottom>
 
-	<tab-bottom :style="style2">
-		<tab-item><i class="fa fa-lg fa-phone"></i></tab-item>
-		<tab-item><i class="fa fa-lg fa-comment"></i></tab-item>
-		<tab-item selected><i class="fa fa-lg fa-user"></i></tab-item>
-		<tab-item><i class="fa fa-lg fa-camera"></i></tab-item>
+	<tab-bottom :on-select="tabChangeHandle" :style="style2">
+		<tab-item event-key="1"><i class="fa fa-lg fa-phone"></i></tab-item>
+		<tab-item event-key="2"><i class="fa fa-lg fa-comment"></i></tab-item>
+		<tab-item event-key="3" selected><i class="fa fa-lg fa-user"></i></tab-item>
+		<tab-item event-key="4"><i class="fa fa-lg fa-camera"></i></tab-item>
 	</tab-bottom>
 
-	<tab-bottom :style="style3" split>
-		<tab-item>电话</tab-item>
-		<tab-item>短信</tab-item>
-		<tab-item>我</tab-item>
-		<tab-item>照相</tab-item>
+	<tab-bottom :on-select="tabChangeHandle" :style="style3" split>
+		<tab-item event-key="电话">电话</tab-item>
+		<tab-item event-key="短信">短信</tab-item>
+		<tab-item event-key="我">我</tab-item>
+		<tab-item event-key="照相">照相</tab-item>
 	</tab-bottom>
 
-	<tab-bottom :style="style" cascade>
-		<tab-item>
+	<tab-bottom :on-select="tabChangeHandle" :style="style" cascade>
+		<tab-item event-key="1-电话">
 			<i class="fa fa-lg fa-phone"></i>
 			<span>电话</span>
 		</tab-item>
-		<tab-item selected>
+		<tab-item event-key="2-短信" selected>
 			<i class="fa fa-lg fa-comment"></i>
 			<span>短信</span>
 		</tab-item>
-		<tab-item>
+		<tab-item event-key="3-我">
 			<i class="fa fa-lg fa-user"></i>
 			<span>我</span>
 		</tab-item>
-		<tab-item>
+		<tab-item event-key="4-照相">
 			<i class="fa fa-lg fa-camera"></i>
 			<span>照相</span>
 		</tab-item>
@@ -67,6 +67,11 @@
 				style3: {
 					top: '255px'
 				}
+			}
+		},
+		methods: {
+			tabChangeHandle(key){
+				console.log(`${key}:被选中了！`);
 			}
 		}
 	};
